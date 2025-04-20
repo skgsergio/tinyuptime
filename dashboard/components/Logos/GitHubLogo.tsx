cf9134fbd73a388846d9bb6819ed90aa92002aec
Sergio Conde <skgsergio@gmail.com>
2025-04-20 22:34:48 +0200

    Fix SVG components

--- dashboard/components/Logos/GitHubLogo.tsx
+++ dashboard/components/Logos/GitHubLogo.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const GitHubLogo: React.FC<React.SVGProps<SVGElement>> = (props) => (
+const GitHubLogo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96" {...props}>
     <path
       fill="currentColor"
